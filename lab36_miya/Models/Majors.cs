a249e61808d666b031aff3217468424bce1d37b0
miyaluc <srlucas3@gmail.com>
2017-11-02 09:41:02 -0700

    added Majors model and updated RequiredCoursework model

--- lab36_miya/Models/Majors.cs
+++ lab36_miya/Models/Majors.cs
@@ -1,30 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace lab36_miya.Models
 {
-    public class RequiredCoursework
+    public class Majors
     {
         public int ID
         {
             get; set;
         }
 
-        [MinLength(4)]
-        public string Class
+        public string Name
         {
             get; set;
         }
 
-        public bool IsComplete
-        {
-            get; set;
-        }
-
-        public int ListID
+        public List<RequiredCoursework> Courses
         {
             get; set;
         }
